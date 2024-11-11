70a574b2b1b286d510878599ab2fae8f692ee987
Ivan Barbosa <ivanjulio.ij@gmail.com>
2024-11-10 23:19:45 -0300

    Criadas as classes ContaCorrente & ContaPoupança e os respectivos métodos de teste no Menu.

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -2,6 +2,8 @@ package conta;
 
 import java.util.Scanner;
 import conta.model.Conta;
+import conta.model.ContaCorrente;
+import conta.model.ContaPoupança;
 import conta.util.Cores;
 
 public class Menu {
@@ -16,6 +18,21 @@ public class Menu {
 				c1.depositar(5000.0f);
 				c1.visualizar();
 
+				// Teste da Classe Conta Corrente
+				ContaCorrente cc1 = new ContaCorrente(1, 123, 1, "José da Silva", 0.0f, 1000.0f);
+				cc1.visualizar();
+				cc1.sacar(12000.0f);
+				cc1.visualizar();
+				cc1.depositar(5000.0f);
+				cc1.visualizar();
+				
+		        // Teste da Classe Conta Poupança
+				ContaPoupança cp1 = new ContaPoupança(2, 123, 2, "Maria dos Santos", 100000.0f, 15);
+				cp1.visualizar();
+		        cp1.sacar(1000.0f);
+				cp1.visualizar();
+				cp1.depositar(5000.0f);
+				cp1.visualizar();
 				
 		Scanner leia = new Scanner(System.in);
 
